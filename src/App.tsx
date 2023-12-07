dc2e3231e9777fd2c6466d27e8eb01acbb701b75
iamKarthikRam <karthikbeece@gmail.com>
2023-12-07 13:07:48 +0530

    Install basic dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export const App: FC<{ name: string }> = ({ name }) => {
   return (
     <div>
       <h1>Hello {name}!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <p>Start editing to see somenpm ru magic happen :)</p>
     </div>
   );
 };
